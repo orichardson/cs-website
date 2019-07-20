46e55f7a631d94d00e4a58ed7b108ef97d0a0a4b
Oliver Richardson <oli@cs.cornell.edu>
2019-07-20 19:56:35 -0400

    testing push cannot log into cslinux

--- test.php
+++ test.php
@@ -9,7 +9,7 @@
 	<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 
-    <base href="cs.cornell.edu/~oli" target="_blank">
+    <!-- <base href="cs.cornell.edu/~oli" target="_blank"> -->
     <!--link rel="stylesheet" type="text/css" href="/stylesheets/svgtext.css" /-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
